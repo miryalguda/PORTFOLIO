3e777f3a4b422d829cd57d91d2fe9629b6226dad
miryalguda <137521245+miryalguda@users.noreply.github.com>
2023-09-25 16:31:42 -0400

    Add files via upload

---
+++ PORTFOLIO2js.js
@@ -0,0 +1,14 @@
+// multiple dropdown js //
+
+    $(document).ready(function(){
+      $('.dropdown-submenu a.test').on("click", function(e){
+        $(this).next('ul').toggle();
+        e.stopPropagation();
+        e.preventDefault();
+      });
+    });
+
+//multiple dropdown js end //
+//Option 1: Bootstrap Bundle with Popper //
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
